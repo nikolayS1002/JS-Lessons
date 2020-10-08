af613d5650012bcf783ddfbfd94910fca2ac2a63
Nikolay S <nikolay1002@yandex.ru>
2020-10-08 13:04:18 +0300

    подготовка ко второму занятию

---
+++ CaroLesson 2/main.js
@@ -0,0 +1,19 @@
+/*function sum (a, b){
+    return a + b
+}
+
+alert(sum(10, 2) > sum(1, 4))*/
+
+// 1)
+let tf;
+let tc = 105;
+alert("Температура по цельсию: " + tc + ", по фарингейту: " + (tf = (9 / 5) * tc + 32));
+
+// 2)
+let admin;
+let name = "Василий";
+admin = name;
+alert(name);
+
+// 3)
+alert(1000 + "108");
